21dbc734dcd17cb66327daf524cdae5f1745ec69
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-08 17:31:57 +0100

    Preference and User context

---
+++ contexts/PreferenceContext.jsx
@@ -0,0 +1,29 @@
+import { useEffect, createContext, useContext, useReducer } from 'react';
+
+import reducer from './prefrenceReducer';
+const PreferenceContext = createContext({});
+
+export function PreferenceProvider({children}) {
+    const [preferences, dispatch] = useReducer(reducer, {
+        isOpen: false,
+        type: 'words',
+        time: '15',
+      });
+    
+      useEffect(() => {
+        if (typeof window !== undefined) {
+          const type = window.localStorage.getItem('type');
+          const time = window.localStorage.getItem('time');
+          if (type) dispatch({ type: 'SET_TYPE', payload: type });
+          if (time) dispatch({ type: 'SET_TIME', payload: time });
+        }
+      }, []);
+    
+      return (
+        <PreferenceContext.Provider value={{ preferences, dispatch }}>
+          {children}
+        </PreferenceContext.Provider>
+      );
+}
+
+export const usePreferenceContext = () => useContext(PreferenceContext)
