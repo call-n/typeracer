fd22e9c5e4b377bf9f07cb5d6bad486b78fb20f0
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-08 17:33:36 +0100

    Account and Leaderboard, need finetuning

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router';
+import AnimatedTransition from '../components/Layout/AnimatedTransition';
+import {
+  Flex,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  Box,
+} from '@chakra-ui/react';
+import { useUserContext } from '../contexts/UserContext';
+import AccountDetails from '../components/AccountDetails';
 
-const account = () => {
+
+const Account = () => {
+    const {
+        currentUser,
+    } = useUserContext();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(!currentUser){
+            router.push('/login')
+        }
+    },[currentUser, router])
+
   return (
-    <div>account</div>
+    <AnimatedTransition>
+      <Container maxW={'5xl'} height='85vh' display='flex' justifyContent='center' alignItems='center'>
+      <Stack
+        textAlign={'center'}
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={'header'}>
+            <Text
+              color={'gray.400'}
+              fontWeight={300}
+              fontSize={'2xl'}>
+              Welcome back
+            </Text>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+              {currentUser?.displayName}
+            </Heading>
+          </Box>
+            <Text
+            color={'gray.400'}
+            fontSize={'2xl'}
+            fontWeight={'300'}>
+            Hope you have had a nice time here!
+            </Text>
+            {currentUser && (
+                <AccountDetails />
+            )}
+        </Stack>
+      </Stack>
+    </Container>
+    </AnimatedTransition>
   )
 }
 
-export default account
+export default Account
--- pages/leaderboard.jsx
+++ pages/leaderboard.jsx
@@ -1,9 +1,168 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import AnimatedTransition from '../components/Layout/AnimatedTransition';
+import {
+  Flex,
+  Box,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  Button,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  SimpleGrid,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
+import { db } from "../firebase";
+import { collection, query, where, getDocs, orderBy, limit, onSnapshot } from "firebase/firestore";
 
-const leaderboard = () => {
+const Leaderboard = () => {
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
+    const ref = query(collection(db, "scoreboard"), orderBy("wpm", "desc"), limit(5));
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(ref, (snapshot) => {
+            let results = []
+            snapshot.docs.forEach(doc => {
+              results.push({ id: doc.id , ...doc.data()})
+            })
+            
+            setDocuments(results)
+            setError(null)
+          }, error => {
+            console.log(error)
+            setError('could not fetch the data')
+          })
+      
+        return () => unsubscribe()
+    },[ref])
+
+    console.log(documents)
+
   return (
-    <div>leaderboard</div>
+    <AnimatedTransition>
+      <Container maxW={'5xl'} height='85vh' display='flex' justifyContent='center' alignItems='center'>
+      <Stack
+        textAlign={'center'}
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+        >
+        <Box>
+            <Box padding="5rem">
+                <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+                    All-time Leaderboards
+                </Heading>
+            </Box>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                <Box>
+                    <Text
+                        fontSize={{ base: '18px', lg: '20px' }}
+                        color={'green.300'}
+                        fontWeight={'500'}
+                        mb={'4'}
+                        textAlign='left'>
+                        Time 15 s
+                    </Text>
+                    <TableContainer>
+                        <Table size='md' variant='simple'>
+                            <TableCaption>Leaderboard for time 15s</TableCaption>
+                            <Thead>
+                            <Tr border="none">
+                                <Th color='gray.500'>#</Th>
+                                <Th paddingLeft='0' color='gray.500'>name</Th>
+                                <Th textAlign='right' color='gray.500'>wpm <br /> <Box color='gray.600'>Accuracy%</Box></Th>
+                                <Th isNumeric color='gray.500'>date</Th>
+                            </Tr>
+                            </Thead>
+                            <Tbody>
+                            {documents && (
+                                documents.map((doc, index) => (
+                                <Tr key={index+1}>
+                                    <Td>{index+1}</Td>
+                                    <Td paddingLeft='0'>{doc.name}</Td>
+                                    <Td textAlign='right'>{doc.wpm} <br /> <Box color='gray.500'>{doc.acc}%</Box></Td>
+                                    <Th textAlign='right' color='gray.500'>28 Oct 2023 <br /> <Box color='gray.600'>19.07</Box></Th>
+                                </Tr>
+                                ))
+                            )}
+                            </Tbody>
+                            <Tfoot>
+                            <Tr>
+                                <Th color='gray.500'>#</Th>
+                                <Th paddingLeft='0' color='gray.500'>name</Th>
+                                <Th textAlign='right' color='gray.500'>wpm <br /> <Box color='gray.600'>Accuracy%</Box></Th>
+                                <Th isNumeric color='gray.500'>date</Th>
+                            </Tr>
+                            </Tfoot>
+                        </Table>
+                    </TableContainer>
+                </Box>
+                <Box>
+                    <Text
+                        fontSize={{ base: '18px', lg: '20px' }}
+                        color={'green.300'}
+                        fontWeight={'500'}
+                        mb={'4'}
+                        textAlign='left'>
+                        Time 60 s
+                    </Text>
+                    <TableContainer>
+                        <Table size='md' variant='simple'>
+                            <TableCaption>Leaderboard for time 60s</TableCaption>
+                            <Thead>
+                            <Tr border="none">
+                                <Th color='gray.500'>#</Th>
+                                <Th paddingLeft='0' color='gray.500'>name</Th>
+                                <Th textAlign='right' color='gray.500'>wpm <br /> <Box color='gray.600'>Accuracy%</Box></Th>
+                                <Th isNumeric color='gray.500'>date</Th>
+                            </Tr>
+                            </Thead>
+                            <Tbody>
+                            {documents && (
+                                documents.map((doc, index) => (
+                                <Tr key={index+1}>
+                                    <Td>{index+1}</Td>
+                                    <Td paddingLeft='0'>{doc.name}</Td>
+                                    <Td textAlign='right'>{doc.wpm} <br /> <Box color='gray.500'>{doc.acc}%</Box></Td>
+                                    <Th textAlign='right' color='gray.500'>28 Oct 2023 <br /> <Box color='gray.600'>19.07</Box></Th>
+                                </Tr>
+                                ))
+                            )}
+                            </Tbody>
+                            <Tfoot>
+                            <Tr>
+                                <Th color='gray.500'>#</Th>
+                                <Th paddingLeft='0' color='gray.500'>name</Th>
+                                <Th textAlign='right' color='gray.500'>wpm <br /> <Box color='gray.600'>Accuracy%</Box></Th>
+                                <Th isNumeric color='gray.500'>date</Th>
+                            </Tr>
+                            </Tfoot>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </SimpleGrid>
+        </Box>
+      </Stack>
+    </Container>
+    </AnimatedTransition>
   )
 }
 
-export default leaderboard
+export default Leaderboard
