2f3dab3e23c4b73c21347e4f2f6f71045f2bb9ea
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-16 18:56:14 +0100

    small changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
 'use client';
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { PreferenceProvider } from '../contexts/PreferenceContext';
+import { PreferenceProvider } from '../contexts/Prefrence/PreferenceContext';
 import Layout from '../components/Layout/Layout';
 import { theme } from '../components/theme';
 import { AnimatePresence } from 'framer-motion';
 import UserContext from '../contexts/UserContext';
+import { MultiplayerProvider } from '../contexts/Multiplayer/MultiplayerContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <PreferenceProvider>
         <UserContext>
-          <Layout>
-            <AnimatePresence>
-              <Component {...pageProps} />
-            </AnimatePresence>
-          </Layout>
+          <MultiplayerProvider>
+            <Layout>
+              <AnimatePresence>
+                <Component {...pageProps} />
+              </AnimatePresence>
+            </Layout>
+          </MultiplayerProvider>
         </UserContext>
       </PreferenceProvider>
     </ChakraProvider>
