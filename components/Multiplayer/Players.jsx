f0e87f25bc1319430ecb2542037667315ad155e6
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-18 17:54:35 +0100

    help components for multiplayer

---
+++ components/Multiplayer/Players.jsx
@@ -0,0 +1,78 @@
+import { FaCrown } from 'react-icons/fa';
+
+import { useMultiplayerContext } from '../../contexts/Multiplayer/MultiplayerContext';
+
+import {
+  Box,
+  Flex,
+  Text,
+} from '@chakra-ui/react';
+
+export default function Players() {
+  const {
+    room: {
+      user: { id },
+      players,
+      isPlaying,
+      winner,
+    },
+  } = useMultiplayerContext();
+
+  return (
+    <Box>
+      {players.map((player) =>
+        player.id === id ? (
+          <Flex
+            key={player.id}
+            flex='1'
+            flexDir='col'
+            alignItems='start'
+            gap='2'
+          >
+            <Flex width='100%' alignItems='center' justifyContent='space-between'>
+              <Flex alignItems='center'>
+                {winner === player.id && <FaCrown className='mr-1 text-fg' />}
+                <Text color='green.300'>You</Text>
+                <Text fontSize='sm' marginLeft='.2rem'>
+                  (
+                  {isPlaying
+                    ? 'in game'
+                    : player.isOwner
+                    ? 'owner'
+                    : 'waiting for owner'}
+                  )
+                </Text>
+              </Flex>
+              <Text color='green.300' marginLeft='.2rem'>{player.status.wpm} wpm</Text>
+            </Flex>
+          </Flex>
+        ) : (
+          <Flex
+            key={player.id}
+            flex='1'
+            flexDir='col'
+            alignItems='start'
+            gap='2'
+          >
+            <Flex width='100%' alignItems='center' justifyContent='space-between'>
+              <Flex alignItems='center'>
+                {winner === player.id && <FaCrown className='mr-1 text-fg' />}
+                <Text color='green.300'>{player.username}</Text>
+                <Text fontSize='sm' marginLeft='.2rem'>
+                  (
+                  {isPlaying
+                    ? 'in game'
+                    : player.isOwner
+                    ? 'owner'
+                    : 'waiting for owner'}
+                  )
+                </Text>
+              </Flex>
+              <Text color='green.300' marginLeft='.2rem'>{player.status.wpm} wpm</Text>
+            </Flex>
+          </Flex>
+        )
+      )}
+    </Box>
+  );
+}
---
+++ components/Multiplayer/RoomCode.jsx
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router';
+import { FaCopy } from 'react-icons/fa';
+import { useToast } from '@chakra-ui/react'
+import {
+    Box,
+    Flex,
+  } from '@chakra-ui/react';
+
+export default function Code() {
+  const { query } = useRouter();
+  const toast = useToast();
+
+  return (
+    <Flex
+        pos='relative'
+        zIndex='10'
+        cursor='pointer'
+        alignItems='center'
+        rounded='md'
+        fontWeight='bold'
+        p='.5rem'
+        paddingTop='1.2rem'
+        fontSize='1.4rem'
+        bg='gray.300'
+        color='gray.900'
+        onClick={() =>
+            navigator.clipboard.writeText(query?.id).then(() =>
+            toast({
+                title: 'Copy was succesfull!',
+                description: "Now share it with your friends :)",
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            })
+            )
+        }
+      className='relative z-10 flex cursor-pointer items-center rounded-md bg-hl px-4 pt-5 text-3xl font-bold text-bg'
+    >
+      <Box pos='absolute' top='1' left='2' fontSize='0.8rem'>
+        copy and share
+      </Box>
+      {query?.id ? (
+        query?.id + ' '
+      ) : (
+        <div>que?</div>
+      )}
+      <FaCopy className='ml-2 text-2xl text-bg/80 transition-colors duration-200 hover:text-bg active:text-bg' />
+    </Flex>
+  );
+}
