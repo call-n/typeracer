a432bf194e7098c3bc2f7ae812d5fda0df00acad
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-18 08:15:04 +0100

    pref context

---
+++ contexts/Prefrence/reducer.js
@@ -0,0 +1,24 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_TYPE':
+      if (typeof window !== undefined) {
+        window.localStorage.setItem('type', action.payload);
+      }
+      return {
+        ...state,
+        type: action.payload,
+      };
+    case 'SET_TIME':
+      if (typeof window !== undefined) {
+        window.localStorage.setItem('time', action.payload);
+      }
+      return {
+        ...state,
+        time: action.payload,
+      };
+    default:
+      throw new Error('Unknown action type');
+  }
+};
+
+export default reducer;
