c51bbdba8221cf98eaf5386308438d9c937c58da
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-22 13:02:48 +0100

    FIrebase vars to env, opsie

--- firebase/index.js
+++ firebase/index.js
@@ -1,24 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrXMuh8jc52nnpc4Lnvigt6d2b8Y3DNfE",
-  authDomain: "xwork-70e52.firebaseapp.com",
-  projectId: "xwork-70e52",
-  storageBucket: "xwork-70e52.appspot.com",
-  messagingSenderId: "698332787632",
-  appId: "1:698332787632:web:53687e965e17f9e94e1c75"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-export {
-	app as default,
-	auth,
-	db,
-}
+export { app as default, auth, db };
